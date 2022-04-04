e4f8ff4caf81d055aa8fd8cccd1e510a2e72ecaf
Vivek Ranjan <infovivek.ranjan@gmail.com>
2022-04-04 13:03:59 +0530

    vercel deployment of backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const connect = require('./src/config/db')
-const routes = require('./src/Routes/routes')
+const connect = require('./config/db')
+const routes = require('./Routes/routes')
 const app = express();
 app.use(express.json());
 app.use(cors());
